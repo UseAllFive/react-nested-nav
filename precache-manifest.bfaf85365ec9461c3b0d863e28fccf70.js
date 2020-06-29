fe8ec47381be5b6a0ab013bc728eb2d16745cc73
bretmorris <bret@useallfive.com>
2020-06-29 11:24:25 -0600

    Updates

---
+++ precache-manifest.bfaf85365ec9461c3b0d863e28fccf70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d24a2d2544e62f7ad1886ae69630eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0817d48a32f21455c5fa",
+    "url": "./static/css/main.13f9b455.chunk.css"
+  },
+  {
+    "revision": "ce4a5b2443138aa53798",
+    "url": "./static/js/2.6c8af748.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "./static/js/2.6c8af748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0817d48a32f21455c5fa",
+    "url": "./static/js/main.6c13248b.chunk.js"
+  },
+  {
+    "revision": "2767507d717534c2997f",
+    "url": "./static/js/runtime-main.3701f7d6.js"
+  }
+]);
